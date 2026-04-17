b6b54af841c1bdce2a7ce776a02f96bc26f0e6d1
gabrielborjas <gborjasperez@ucsd.edu>
2026-04-17 14:42:58 -0700

    step 3.1

--- global.js
+++ global.js
@@ -3,9 +3,36 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
+// let navLinks = $$("nav a");
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
 
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/portfolio/";
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/gabrielborjas', title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+
+
