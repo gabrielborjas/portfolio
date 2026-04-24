39c2fa80a7b1e7a9ea06dddc3b14653bffcfaf3b
gabrielborjas <gborjasperez@ucsd.edu>
2026-04-23 18:00:08 -0700

    step 4

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
+const githubData = await fetchGitHubData('gabrielborjas');
+const profileStats = document.querySelector('#profile-stats');
+
